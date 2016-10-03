33160408d5ab565f7f07c09303116dd3e4d851e4
Marco Hemken <mehemken@gmail.com>
2016-10-02 20:42:36 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# Basic flask container
+
+FROM fanoftal2/alpine-flask-base:1
+
+ADD /app/ /home/app/
+WORKDIR /home/app/
+
+EXPOSE 5000
+
+ENTRYPOINT ["python", "app.py"]
---
+++ app/app.py
@@ -0,0 +1,11 @@
+# Basic flask container
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run()
